fc2dcecc842bf520bcbe6970a74168056bea9df8
Peter Gill <peter.gill@townsuite.com>
2015-08-11 10:08:09 -0230

    Open cover chocolatey package

---
+++ README.md
@@ -0,0 +1,4 @@
+CrystalReports2011Runtime
+=============================
+
+Chocolatey package for [OpenCover](https://github.com/OpenCover/opencover) code coverage tool for .net.
---
+++ package/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = "OpenCover"
+$installerType = "msi"
+$silentArgs= "/quiet"
+$url = "https://github.com/OpenCover/opencover/releases/download/4.6.166/opencover.4.6.166.msi"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes @(0)
+
